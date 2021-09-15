18391f658316f8fcf3f813df859df3abd23c8807
xinhao song <xinhao.song@berkeley.edu>
2021-09-15 13:44:45 -0500

    step1 lab2

---
+++ XinhaoSong-Lab2/XinhaoSong-Lab2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  XinhaoSong-Lab2
+//
+//  Created by xinhao.song on 2021/9/15.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
